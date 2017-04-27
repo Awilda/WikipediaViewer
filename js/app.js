a8c54f2a18901a44b1322f1ae5993ec26e86c044
Awilda <awildacantres@gmail.com>
2017-04-26 20:48:50 -0400

    Add css file and begin solution

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	var wikiData = "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json"
+	
+	$("#submit").click(function(){
+		alert("Woooooo!!!");
+	})
+	// var titles = $("#mySearch");
+
+});
